74093ba170e3885aab3f506689242c66446432bb
Denis-Li <den.le.genplan@gmail.com>
2020-12-22 00:04:06 +0100

    work on header 02

--- src/components/HeaderC/HeaderC.js
+++ src/components/HeaderC/HeaderC.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import headerCcss from'./HeaderC.module.css';
+import {NavLink} from 'react-router-dom';
 
 
 const Header = () => {
@@ -7,6 +8,9 @@ const Header = () => {
         <header className={headerCcss.header}>
             <div className={headerCcss.header_img}>
                 <img src="https://www.freelogodesign.org/file/app/client/thumb/4d4ecfde-94ab-4e21-8ffa-8198454be7ea_200x200.png?1605724386399" alt=""/>
+                <div className={headerCcss.loginBlock}>
+                    <NavLink to={'/login'}>Login</NavLink>
+                </div>
             </div>
         </header>
     )

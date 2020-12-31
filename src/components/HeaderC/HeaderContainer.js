1e4237ae1f2a8c07c3f8eecb7bee473306cb4ce6
Denis-Li <den.le.genplan@gmail.com>
2020-12-31 22:21:25 +0100

    work with thunk 06

--- src/components/HeaderC/HeaderContainer.js
+++ src/components/HeaderC/HeaderContainer.js
@@ -3,12 +3,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import HeaderC from './HeaderC';
 import {setAuthUserData} from '../../redux/auth-reducer';
+import { authAPI } from '../../api/api';
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-            withCredentials: true
-        })
+        authAPI.me()
             .then(response => {
                 if (response.data.resultCode === 0) {
                     let {id, login, email} = response.data.data;

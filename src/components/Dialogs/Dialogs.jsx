dd0e79b8532f98086b7cef186553c103edb30922
Denis-Li <den.le.genplan@gmail.com>
2021-01-04 23:50:52 +0100

    work on Login 01

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import dialogCss from './Dialogs.module.css';
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
+import { Redirect } from 'react-router-dom';
 
 const Dialogs = (props) => {
 
@@ -23,6 +24,7 @@ const Dialogs = (props) => {
         props.updateNewMessageBody(body);
     }
 
+    if (!props.isAuth) return <Redirect to={"/login"} />;
     return (
         <div className={dialogCss.dialogs}>
             <div className={dialogCss.dialogsItems}>

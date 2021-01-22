6dfbbe165d706f3de31ee2a90bee9b9dbaae48b7
Denis-Li <den.le.genplan@gmail.com>
2021-01-23 00:46:58 +0100

    work on header and logout

--- src/components/HeaderC/HeaderC.js
+++ src/components/HeaderC/HeaderC.js
@@ -9,8 +9,9 @@ const HeaderC = (props) => {
             <div className={headerCcss.header_img}>
                 <img src="https://www.freelogodesign.org/file/app/client/thumb/4d4ecfde-94ab-4e21-8ffa-8198454be7ea_200x200.png?1605724386399" alt=""/>
                 <div className={headerCcss.loginBlock}>
-                    { props.isAuth ? props.login
-                        : <NavLink to={'/login'}>Login</NavLink> }
+                    { props.isAuth 
+                        ? <div className={headerCcss.logout}>{props.login} <button onClick={props.logout}>Log out</button></div>
+                        : <NavLink className={headerCcss.login} to={'/login'}>Login</NavLink> }
                 </div>
             </div>
         </header>

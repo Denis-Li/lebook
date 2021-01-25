fb8089df204e343b75ec5fa20da1873845dfa9a8
Denis-Li <den.le.genplan@gmail.com>
2021-01-25 23:54:23 +0100

    work on app-reducer 01

--- src/App.js
+++ src/App.js
@@ -8,16 +8,22 @@ import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/HeaderC/HeaderContainer';
 import LoginPage from './components/Login/Login';
 import { connect } from 'react-redux';
-import {getAuthUserData} from './redux/auth-reducer';
+import {initializeApp} from './redux/app-reducer';
 import { compose } from 'redux';
+import Preloader from './components/common/Preloader/Preloader';
 
 
 
 class App extends Component {
   componentDidMount() {
-    this.props.getAuthUserData();
+    this.props.initializeApp();
   }
   render() {
+    if (!this.props.initialized) {
+      return <Preloader />
+
+    }
+
     return (
       <div className='app-wrapper'>
         <HeaderContainer />
@@ -34,6 +40,10 @@ class App extends Component {
 
 }
 
+const mapStateToProps = (state) => ({
+  initialized: state.app.initialized
+})
+
 export default compose(
   withRouter, 
-  connect (null, {getAuthUserData})) (App);
+  connect (mapStateToProps, {initializeApp})) (App);

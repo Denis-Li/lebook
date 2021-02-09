5f0ae25a56dceeca6da5c455c47de84f4f262e56
Denis-Li <den.le.genplan@gmail.com>
2021-02-09 02:00:58 +0100

    work on profile-reduser test 02

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,15 +1,46 @@
-import profileReducer, { addPostActionCreator } from "./profile-reducer";
+import profileReducer, { addPostActionCreator, deletePost } from "./profile-reducer";
 import React from 'react';
 
 
-it('new post should be added', () => {
-    let action = addPostActionCreator("denisli.info");
-    let state = {
-        posts: [
-            {id: 1, message: 'Hi, how are you?', likesCount: 12},
-            {id: 2, message: 'It\'s my first post', likesCount: 10},
-        ]
-    };
-    let newState = profileReducer(state, action);
+let state = {
+    posts: [
+        {id: 1, message: 'Hi, how are you?', likesCount: 12},
+        {id: 2, message: 'It\'s my first post', likesCount: 10},
+    ]
+};
 
+it('length of post should be incremented', () => {
+    // 1. test data
+    let action = addPostActionCreator("denisli.info");
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect (newState.posts.length).toBe(3);
+});
+it('message of new post should be correct', () => {
+    // 1. test data
+    let action = addPostActionCreator("denisli.info");
+    
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect (newState.posts[2].message).toBe("denisli.info");
+});
+it('after deteling length of masseges should be decrement', () => {
+    // 1. test data
+    let action = deletePost(1);
+    
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect (newState.posts.length).toBe(1);
+});
+it(`after deteling length should't be decrement if id is incorrect`, () => {
+    // 1. test data
+    let action = deletePost(1000);
+    
+    // 2. action
+    let newState = profileReducer(state, action);
+    // 3. expectation
+    expect (newState.posts.length).toBe(2);
 });

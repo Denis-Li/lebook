51a64d4da090b01f15cb46e8e6d33a7fde2a77eb
Denis-Li <den.le.genplan@gmail.com>
2021-03-02 00:23:54 +0100

    profileStatus test 01

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import SamurajJSApp from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<SamurajJSApp />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<SamurajJSApp />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });

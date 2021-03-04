092a1845c2de0f80f692a8aa0dbef96555fb4d52
Denis-Li <den.le.genplan@gmail.com>
2021-03-05 00:37:09 +0100

    test 06

--- src/components/Profile/ProfileInfo/ProfileStatus.test.js
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.js
@@ -12,7 +12,7 @@ describe("ProfileStatus component", () => {
       const component = create(<ProfileStatus status="it-kamasutra.com" />);
       const root = component.root;
       let span = root.findByType("span")
-      expect(span).not.toBeNull(1);
+      expect(span).not.toBeNull();
     });
     test("after creation <input> shouldn't be displayed", () => {
       const component = create(<ProfileStatus status="it-kamasutra.com" />);
@@ -28,5 +28,20 @@ describe("ProfileStatus component", () => {
       const root = component.root;
       let span = root.findByType("span")
       expect(span.children[0]).toBe("it-kamasutra.com");
+    });
+    test("input should be displayed in editMode instead of span", () => {
+      const component = create(<ProfileStatus status="it-kamasutra.com" />);
+      const root = component.root;
+      let span = root.findByType("span");
+      span.props.onDoubleClick();
+      let input = root.findByType("input");
+      expect(input.props.value).toBe("it-kamasutra.com");
+    });
+    test("callback should be called", () => {
+      const mockCallback = jest.fn();
+      const component = create(<ProfileStatus status="it-kamasutra.com" updateStatus={() => {}} />);
+      const instance = component.getInstance();
+      instance.deactivateEditMode();
+      expect(mockCallback.mock.calls.length).toBe(1);
     });
 });

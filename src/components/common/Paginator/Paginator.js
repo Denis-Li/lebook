93db1909dcd4fbe059b8bb3447d8f208f4a625ba
Denis-Li <den.le.genplan@gmail.com>
2021-03-06 01:18:42 +0100

    work on paginator 03

--- src/components/common/Paginator/Paginator.js
+++ src/components/common/Paginator/Paginator.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import usersCss from './Paginator.module.css';
 
 
-let Paginator = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
+let Paginator = ({totalItemsCount, pageSize, currentPage, onPageChanged, portionSize = 10}) => {
     
-    let pagesCount = Math.ceil (totalUsersCount / pageSize);
+    let pagesCount = Math.ceil (totalItemsCount / pageSize);
     let pages = [];
     for (let i=1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    return <div>
+
+    let portionCount = Math.ceil(pagesCount / portionSize);
+    let [portionNumber, setPortionNumber] = useState(1);
+    let leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+    let rightPortionPageNumber = portionNumber * portionSize;
+
+
+    return <div className={usersCss.paginator}>
+        {portionNumber > 1 &&
+        <button onClick={ () => { setPortionNumber(portionNumber - 1)}}>PREV</button>}
+
             {pages.map(p => {
                 return <span className={currentPage === p && usersCss.selectedPage}
                 onClick={(e) => {onPageChanged(p);}}>{p}</span>

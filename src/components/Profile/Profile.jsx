e55fdf43f59c11b01be9dd5b85fc6776f1e505ae
Denis-Li <den.le.genplan@gmail.com>
2021-03-21 01:55:33 +0100

    work with savePhoto

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,7 +10,11 @@ const Profile = (props) => {
 
     return(
         <div>
-            <ProfileInfo isOwner={props.isOwner} profile={props.profile} status={props.status} updateStatus={props.updateStatus} /> 
+            <ProfileInfo savePhoto={props.savePhoto} 
+            isOwner={props.isOwner} 
+            profile={props.profile} 
+            status={props.status} 
+            updateStatus={props.updateStatus} /> 
             <MyPostsContainer />
         </div>
     )

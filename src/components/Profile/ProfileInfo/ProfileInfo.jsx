84b32c0aa3bd155614cfdc918d28ed8bd9e8dc03
Denis-Li <den.le.genplan@gmail.com>
2021-03-23 23:53:07 +0100

    work on ProfileInfo 06

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -24,25 +24,7 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
                 <img src={profile.photos.large || userPhoto} alt="" className={profileInfoCss.imgBlock} />
                 { isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>}
 
-                <div>
-                    <div>
-                        <b>Full name</b>: {profile.fullName}
-                    </div>
-                    <div>
-                        <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
-                    </div>
-                    {profile.lookingForAJob && 
-                        <div>
-                            <b>My professional skills</b>: {profile.lookingForAJobDesvription}
-                        </div>
-                    }
-                    <div>
-                        <b>About me</b>: {profile.aboutMe}
-                    </div>
-                    <div>
-                        {/* <b>Contacts</b>: {profile.contacts} */}
-                    </div>
-                </div>
+                <ProfileData profile={profile} />
 
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus} />
                 <div>{profile.contacts.instagram}</div>
@@ -52,8 +34,32 @@ const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     )
 }
 
+const ProfileData = ({profile}) => {
+    return <div>
+            <div>
+                <b>Full name</b>: {profile.fullName}
+            </div>
+            <div>
+                <b>Looking for a job</b>: {profile.lookingForAJob ? "yes" : "no"}
+            </div>
+            {profile.lookingForAJob && 
+                <div>
+                    <b>My professional skills</b>: {profile.lookingForAJobDesvription}
+                </div>
+            }
+            <div>
+                <b>About me</b>: {profile.aboutMe}
+            </div>
+            <div>
+                <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
+                    return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]} />
+                })}
+            </div>
+    </div>
+}
+
 const Contact = ({contactTitle, contactValue}) => {
-    return <div><b>{contactTitle}</b>: {contactValue}</div>
+    return <div className={profileInfoCss.contact}><b>{contactTitle}</b>: {contactValue}</div>
 }
 
 export default ProfileInfo;
